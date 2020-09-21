f7db5b618179c3a81eb3fea10b62adcf5c4e3de9
kyeolhee <ggood929@naver.com>
2020-09-21 12:26:16 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #define NUMBER 256
 #define PLUS 257
@@ -24,20 +27,36 @@ int go_to[12][3] = {
 int prod_left[7] = { 0, EXPRESSION, EXPRESSION, TERM, TERM, FACTOR, FACTOR };
 int prod_length[7] = { 0, 3, 1, 3, 1, 3, 1 };
 
+typedef struct _Type {
+	// DataType = 0 is int, DataType = 1 is float;
+	int DataType;
+	int i_num;
+	float f_num;
+}Type;
+
 int stack[1000];
-int value[1000];
+Type value[1000];
 int top = -1;
 int sym;
 char yytext[32];
-int yyIval;
+Type yyIval;
+// time = Position of the expression
+int time = 0;
+// warning = true is int+float , warning = false is only int or float
+bool warning = false;
 
 void push(int);
 void reduce(int);
+void shift(int i);
 void yyerror();
 void lex_error();
+int yyparse();
+int yylex();
 
-void main() {
+int main() {
+	printf("Input : ");
 	yyparse();
+	return 0;
 }
 
 int yyparse() {
@@ -46,8 +65,15 @@ int yyparse() {
 	sym = yylex();
 	do {
 		i = action[stack[top]][sym - 256];
-		if (i == ACC)
-			print("success! \n");
+		if (i == ACC) {
+			if (value[top].DataType == 0)
+				printf("Result :  %d\n ", value[top].i_num);
+			else
+				printf("Result :  %f\n ", value[top].f_num);
+
+			if (warning == true)
+				printf("It is a mixture of int and float. (at %d)\n", time);
+		}
 		else if (i > 0)
 			shift(i);
 		else if (i < 0)
@@ -58,65 +84,39 @@ int yyparse() {
 }
 
 int yylex() {
-	//생략
+	static char ch = ' ';
 	int i = 0;
+
+	for (i = 0; i < 32; i++) {
+		yytext[i] = ' ';
+	}
+
+	i = 0;
+	yyIval.DataType = 0;
+	yyIval.i_num = 0;
+	yyIval.f_num = 0;
+	while (ch == ' ' || ch == '\t')
+		ch = getchar();
 	if (isdigit(ch)) {
 		do {
 			yytext[i++] = ch;
 			ch = getchar();
 		} while (isdigit(ch));
-
-		yytext[i] = 0;
-		yyIval = atoi(yytext);
-		return(NUMBER);
-	}
-	//생략
-}
-
-void push(int i) {
-	top++;
-	stack[top] = i;
-}
-
-void shift(int i) {
-	push(i);
-	value[top] == yyIval;
-	sym = yylex();
-}
-
-void reduce(int i) {
-	int old_top;
-	top -= prod_length[i];
-	old_top = top;
-	push(go_to[stack[old_top]][prod_left[i]]);
-	//뭔가 생략됐다는거같음
-	switch (i) {
-	case 1: value[top] = value[old_top + 1] + value[old_top + 3];
-		break;
-	case 2: value[top] = value[old_top + 1];
-		break;
-	case 3: value[top] = value[old_top + 1] * value[old_top + 3];
-		break;
-	case 4: value[top] = value[old_top + 1];
-		break;
-	case 5: value[top] = value[old_top + 2];
-		break;
-	case 6: value[top] = value[old_top + 1];
-		break;
-	default: yyerror("parsing table error");
-		break;
-	}
-}
-
-void yyerror() {
-	static char ch = ' ';
-	int i = 0;
-	while (ch == ' ' || ch == '\t' || ch == '\n')
-		ch = getchar();
-	if (isdigit(ch)) {
-		do
-			ch = getchar();
-		while (isdigit(ch));
+		if (ch == '.') {
+			do {
+				yytext[i++] = ch;
+				ch = getchar();
+			} while (isdigit(ch));
+			yyIval.DataType = 1;
+			yyIval.f_num = atof(yytext);
+			if (warning == false)
+				time++;
+			return (NUMBER);
+		}
+		yyIval.DataType = 0;
+		yyIval.i_num = atoi(yytext);
+		if (warning == false)
+			time++;
 		return (NUMBER);
 	}
 	else if (ch == '+') {
@@ -135,12 +135,94 @@ void yyerror() {
 		ch = getchar();
 		return(RPAREN);
 	}
-	else if (ch == 'EOF') {
-		ch = getchar();
+	else if (ch == '\n') {
 		return(END);
 	}
 	else
 		lex_error();
+}
+
+void push(int i) {
+	top++;
+	stack[top] = i;
+}
+
+void shift(int i) {
+	push(i);
+	value[top] = yyIval;
+	sym = yylex();
+}
+
+void reduce(int i) {
+	int old_top;
+	top -= prod_length[i];
+	old_top = top;
+	push(go_to[stack[old_top]][prod_left[i]]);
+	switch (i) {
+	case 1:
+		if (value[old_top + 1].DataType == 0) {
+			if (value[old_top + 3].DataType == 0) {
+				value[top].DataType = 0;
+				value[top].i_num = value[old_top + 1].i_num + value[old_top + 3].i_num;
+			}
+			else {
+				warning = true;
+				value[top].DataType = 1;
+				value[top].f_num = value[old_top + 1].i_num + value[old_top + 3].f_num;
+			}
+		}
+		else {
+			if (value[old_top + 3].DataType == 1) {
+				value[top].DataType = 1;
+				value[top].f_num = value[old_top + 1].f_num + value[old_top + 3].f_num;
+			}
+			else {
+				warning = true;
+				value[top].DataType = 1;
+				value[top].f_num = value[old_top + 1].f_num + value[old_top + 3].i_num;
+			}
+		}
+		break;
+	case 2: value[top] = value[old_top + 1];
+		break;
+	case 3:
+		if (value[old_top + 1].DataType == 0) {
+			if (value[old_top + 3].DataType == 0) {
+				value[top].DataType = 0;
+				value[top].i_num = value[old_top + 1].i_num * value[old_top + 3].i_num;
+			}
+			else {
+				warning = true;
+				value[top].DataType = 1;
+				value[top].f_num = value[old_top + 1].i_num * value[old_top + 3].f_num;
+			}
+		}
+		else {
+			if (value[old_top + 3].DataType == 1) {
+				value[top].DataType = 1;
+				value[top].f_num = value[old_top + 1].f_num * value[old_top + 3].f_num;
+			}
+			else {
+				warning = true;
+				value[top].DataType = 1;
+				value[top].f_num = value[old_top + 1].f_num * value[old_top + 3].i_num;
+			}
+		}
+		break;
+	case 4: value[top] = value[old_top + 1];
+		break;
+	case 5: value[top] = value[old_top + 2];
+		break;
+	case 6: value[top] = value[old_top + 1];
+		break;
+	default: yyerror("parsing table error");
+		break;
+	}
+}
+
+void yyerror() {
+	printf("syntax error\n");
+	exit(1);
 }
 
 void lex_error() {
